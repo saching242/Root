63a0113db5d5936aa2969fa7ccda8611fab27941
user <user@hp>
2019-09-20 18:20:08 +0530

    added new app

---
+++ GradleFirstApp/src/main/java/com/cg/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.cg.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class DemoController {
+
+	   @RequestMapping("/")
+	    public String welcome(Map<String, Object> model) {
+	        return "welcome";
+	    }
+}
